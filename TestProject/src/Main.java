40f2768e975660839315b2f4a7a8fd91b9176cfb
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-09-22 17:44:48 +0900

    .

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -1,8 +1,42 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("Chrome 3");
-		System.out.println("4");
+		int a = (int) (Math.random() * 45) + 1;
+		int b = (int) (Math.random() * 45) + 1;
+		int c = (int) (Math.random() * 45) + 1;
+		int d = (int) (Math.random() * 45) + 1;
+		int e = (int) (Math.random() * 45) + 1;
+		int f = (int) (Math.random() * 45) + 1;
+
+		System.out.print("로또 번호 : ");
+		System.out.print(a + " ");
+		System.out.print(b + " ");
+		System.out.print(c + " ");
+		System.out.print(d + " ");
+		System.out.print(e + " ");
+		System.out.print(f + " ");
+
+		if (a < 46) {
+
+		} else if (a == b) {
+			b = (int) (Math.random() * 45) + 1;
+			System.out.print(b);
+		} else if (b == c) {
+			c = (int) (Math.random() * 45) + 1;
+			System.out.print(c);
+		} else if (c == d) {
+			d = (int) (Math.random() * 45) + 1;
+			System.out.print(d);
+			return;
+		} else if (d == e) {
+			e = (int) (Math.random() * 45) + 1;
+			System.out.print(e);
+			return;
+		} else if (e == f) {
+			f = (int) (Math.random() * 45) + 1;
+			System.out.print(f);
+			return;
+		} else {
+
+		}
 	}
 }
